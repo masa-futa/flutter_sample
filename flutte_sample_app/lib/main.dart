b579160798c71762a22c558819de15539fde60ef
masa-futa <masakunn1215@gmail.com>
2021-08-16 23:07:38 +0900

    [Add] Riverpod + Hooksを使用し状態管理を追加

--- flutte_sample_app/lib/main.dart
+++ flutte_sample_app/lib/main.dart
@@ -4,9 +4,23 @@ import 'package:flutter/material.dart';
 import 'package:flutte_sample_app/main_model.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+final counteProvider = StateNotifierProvider((_) => Counter());
+
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
+  void increment() => state++;
+}
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      ProviderScope(
+        child: RiverPodTest()
+      ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,6 +32,30 @@ class MyApp extends StatelessWidget {
       home: MyAppStateful(),
     );
   }
+}
+
+class RiverPodTest extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final state = useProvider(counteProvider.state);
+    final counter = useProvider(counteProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("title: RiverPodTest"),
+        ),
+        body: Center(
+          child: Text(state.toString()),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => counter.increment(),
+        ),
+      ),
+    );
+  }
+
 }
 
 class MyAppStateful extends StatefulWidget {
@@ -34,32 +72,35 @@ class MyAppState extends State<MyAppStateful> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<MainModel>(
-      create: (_) => MainModel(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Sample App"),
-        ),
-        body: Consumer<MainModel>(
-          builder: (context, model, snapshot) {
-            return Center(
-              child: Column(
-                children: [
-                  Text(model.countText.toString(), style: TextStyle(fontSize: 30)),
-                  countButton(model),
-                  methodButton(),
-                  centerintrinsicWidth(),
-                  padding(),
-                  row(),
-                  // listView()
-                ],
-              ),
-            );
-          }
-        )
-      ),
-    );
+    return Text("");
+    /// 以下はprovider model
+    // return ChangeNotifierProvider<MainModel>(
+    //   create: (_) => MainModel(),
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text("Sample App"),
+    //     ),
+    //     body: Consumer<MainModel>(
+    //       builder: (context, model, snapshot) {
+    //         return Center(
+    //           child: Column(
+    //             children: [
+    //               Text(model.countText.toString(), style: TextStyle(fontSize: 30)),
+    //               countButton(model),
+    //               methodButton(),
+    //               centerintrinsicWidth(),
+    //               padding(),
+    //               row(),
+    //               // listView()
+    //             ],
+    //           ),
+    //         );
+    //       }
+    //     )
+    //   ),
+    // );
   }
+
   /// カウントアップボタン
   Widget countButton(MainModel model) {
     return RaisedButton(
